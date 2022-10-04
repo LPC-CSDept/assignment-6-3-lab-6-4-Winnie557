1059ae68c20280f680e7c11053d710b1ad0724c1
Winnie Chen <wxchen0040@zonemail.clpccd.edu>
2022-10-03 23:10:48 -0700

    1 commit

---
+++ A63.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int getSum(int);
+int main(){
+    int n, sum;
+    do{
+        cout << "Enter the number of input: ";
+        cin >> n;
+        if(n != -1){
+            sum = getSum(n);
+            cout << "The sum is " << sum << endl;
+        }
+    }while(n != -1);
+    return 0;
+}
+int getSum(int n){
+    int number;
+    int max = -1, min = 10000000, sum =0;
+    cout << "Enter " << n << "elements: \n";
+    for(int i=0; i < n; i++){
+        cin >> number;
+        sum += number;
+        if(max < number){
+            max = number;
+        }
+        if(min > number){
+            min = number;
+        }
+    }
+sum = sum - max - min;
+return sum;    
+}
